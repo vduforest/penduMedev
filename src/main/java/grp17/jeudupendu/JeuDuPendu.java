65fd764c3439c1f10f7529b816687ca1ce991582
Victor Duforest <vduforest01@gmail.com>
2026-01-12 13:58:16 +0100

    Initial commit

---
+++ src/main/java/grp17/jeudupendu/JeuDuPendu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grp17.jeudupendu;
+
+/**
+ *
+ * @author vdufo
+ */
+public class JeuDuPendu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
